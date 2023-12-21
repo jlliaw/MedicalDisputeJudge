c8a697a03066e820cc007200733af171bbfff307
jlliaw <106467886+jlliaw@users.noreply.github.com>
2023-12-21 22:17:53 +0800

    Add files via upload

---
+++ 01_getJudgement.py
@@ -0,0 +1,119 @@
+# 司法院裁判書系統檢索(https://judgment.judicial.gov.tw/FJUD/default.aspx)
+# 取得案號
+# 由案號去司法院openapi取得對應判決
+
+import datetime
+import json
+import os
+from os import walk
+from pathlib import Path, PurePath
+from urllib.parse import unquote
+
+import requests
+from bs4 import BeautifulSoup
+
+#get parameter __VIEWSTATE / __VIEWSTATEGENERATOR / __VIEWSTATEENCRYPTED / __EVENTVALIDATION through GET method
+#by using url : https://judgment.judicial.gov.tw/FJUD/Default_AD.aspx
+root_url = 'https://judgment.judicial.gov.tw'
+purl = f"{root_url}/FJUD/Default_AD.aspx"
+soup = None
+
+pcookies = None
+dic_cookies = {}
+
+aspx_VIEWSTATE = ''
+aspx_VIEWSTATEGENERATOR = ''
+aspx_VIEWSTATEENCRYPTED = ''
+aspx_EVENTVALIDATION = ''
+
+with requests.get(purl) as r:
+    if r.status_code == 200:        
+        pcookies = r.cookies #cookies
+
+        for ck in r.cookies: #cookies kvp
+            dic_cookies[ck.name] = ck.value
+        
+        soup = BeautifulSoup(r.text, 'html.parser')            
+        aspx_VIEWSTATE = '' if soup.select_one("#__VIEWSTATE") == None else soup.select_one("#__VIEWSTATE")["value"]
+        aspx_VIEWSTATEGENERATOR = '' if soup.select_one("#__VIEWSTATEGENERATOR") == None else soup.select_one("#__VIEWSTATEGENERATOR")["value"]
+        aspx_VIEWSTATEENCRYPTED = '' if soup.select_one("#__VIEWSTATEENCRYPTED") == None else soup.select_one("#__VIEWSTATEENCRYPTED")["value"]
+        aspx_EVENTVALIDATION = '' if soup.select_one("#__EVENTVALIDATION") == None else soup.select_one("#__EVENTVALIDATION")["value"]
+
+
+#get data by using POST method
+
+payload = {}
+
+''''''
+payload["__VIEWSTATE"] = aspx_VIEWSTATE
+payload["__VIEWSTATEGENERATOR"] = aspx_VIEWSTATEGENERATOR
+payload["__VIEWSTATEENCRYPTED"] = aspx_VIEWSTATEENCRYPTED
+payload["__EVENTVALIDATION"] = aspx_EVENTVALIDATION
+
+payload["dy1"] = "109"
+payload["dm1"] = "09"
+payload["dd1"] = "30"
+
+payload["dy2"] = "112"
+payload["dm2"] = "10"
+payload["dd2"] = "01"
+
+payload["jud_kw"] = "醫療過失-附民-交簡-交訴-交易-勞安"
+payload["judtype"] = "JUDBOOK"
+payload["whosub"] = "1"
+payload["ctl00$cp_content$btnQry"] = "送出查詢"
+
+
+with requests.post(purl, data = payload) as r:
+    if  r.status_code == 200:
+        soup = BeautifulSoup(r.text, "html.parser")
+
+
+hidQID_value = soup.find(attrs = {"id": "hidQID"}).attrs["value"] #hidQID value for qryresultlst.aspx
+
+#get page links
+purl = f"{root_url}/FJUD/qryresultlst.aspx?q={hidQID_value}"
+
+with requests.get(purl) as r:
+    if  r.status_code == 200:
+        soup = BeautifulSoup(r.text, "html.parser")
+
+#api auth
+api_auth={}
+api_auth["user"] = "your_user_name"
+api_auth["password"] = "your_password"
+res = requests.post(url="https://data.judicial.gov.tw/jdg/api/Auth", data=api_auth)
+jobject = json.loads(res.text)
+token = jobject["Token"]
+
+#option tag的內容->取得分頁
+page_options = soup.find(attrs = {"id": "ddlPage"}).find_all('option')
+for o in page_options:
+    #處理每一分頁
+    #o['value'] / o.text
+    purl = f"{root_url}{o['value']}"
+
+    with requests.get(purl) as r:
+        if  r.status_code == 200:
+            soup = BeautifulSoup(r.text, "html.parser")
+   
+    for p in soup.find_all(attrs = {'id':'hlTitle'}):
+        caseno = p['onclick']        
+        rep_str = ["cookieId", "(", ")", "'"]
+        for s in rep_str:            
+            caseno = caseno.replace(s, "")
+
+        caseno_decode = unquote(caseno.split(',')[0])
+        print(caseno_decode + " : " + p.text)        
+
+        #使用案號從open api取得資料並存成json file
+        jid = caseno_decode
+        payload = {
+            "token":token,
+            "j":jid
+        }
+        res = requests.post(url="https://data.judicial.gov.tw/jdg/api/JDoc", data=payload)
+        judgecontent = json.loads(res.text)
+
+        with open(f"./hospjudge/{jid}.json", 'w', encoding='utf-8') as f:
+            json.dump(judgecontent, f, ensure_ascii=False, indent=4)
---
+++ 02_parse_judgement_keyword.py
@@ -0,0 +1,119 @@
+import json
+import os
+import sys
+import time
+from datetime import datetime, timedelta
+from os import walk
+from pathlib import Path, PurePath
+
+from ckip_transformers import __version__
+from ckip_transformers.nlp import (CkipNerChunker, CkipPosTagger,
+                                   CkipWordSegmenter)
+
+
+def get_all_judgement(listpath):
+    f = []
+    for (dirpath, dirnames, filenames) in walk(listpath):
+        f.extend( dirpath + "/" + f for f in filenames)
+
+    return f
+
+#
+def word_sep(id, article_content):
+    retlist = None
+    text = None
+    keywords = None
+    zipped = None
+    zipped2 = None
+
+    try:
+        text = [article_content]
+        #text.append(article_content)
+        ws = ws_driver(text)
+        pos = pos_driver(ws)
+        #ner = ner_driver(text)
+
+        keywords = set() #避免重複
+        zipped = zip(ws, pos)
+        for z in zipped:
+            zipped2 = zip(z[0], z[1])
+            for w, p in zipped2: #字, 詞性
+                if len(w) > 1 and p.startswith('N'): #名詞系又長度超過1個
+                    keywords.add(w)
+        
+        retlist = list(keywords)
+    except:
+        with open('can_not_proc_id', 'a') as failf:
+            failf.write(f'{id}\n')
+
+
+    return retlist
+
+# Show version
+print(__version__)
+
+# Initialize CKIP drivers
+print("Initializing drivers ... WS")
+ws_driver = CkipWordSegmenter(model="albert-base", device=0) #device=0 by using GPU -1 is not
+print("Initializing drivers ... POS")
+pos_driver = CkipPosTagger(model="albert-base", device=0)
+print("Initializing drivers ... NER")
+ner_driver = CkipNerChunker(model="albert-base", device=0)
+print("Initializing drivers ... all done")
+print()
+
+files = get_all_judgement('./hospjudge')
+outpath = "judgement_keyword"
+
+if os.path.exists(outpath) : os.remove(outpath)
+
+for i, inputf in enumerate(files):
+    #process file      
+    filename = Path(inputf).name
+    print(f"{i+1}/{len(files)} : {filename}")          
+
+    #非事實審級排除排除高院/最高院- 劉邦揚, 我國地方法院刑事醫療糾紛判決的實證分析：2000年至2010, 科技法學評論8卷2期頁257（2011）
+    if filename[2] != 'D':
+        continue
+
+    with open(inputf) as f:
+        object = json.load(f)
+        isbreak = False 
+
+        
+        #break condition, 確認檔案內容是否符合斷詞條件
+        #沒有主文資料 ["JFULLX"]["JFULLCONTENT"]排除
+        if ("JFULLX" not in object):
+            isbreak = True      
+        else:
+            if "JFULLCONTENT" not in object["JFULLX"]:
+                isbreak = True
+        
+        if not isbreak:              
+            keywords_Noun = None                
+            id = None
+            articles = None
+
+            id = object['JID']     
+            articles = object['JFULLX']["JFULLCONTENT"]
+
+            #print(articles)
+            #replace
+            rep_str = ["\r\n", "　", " "]
+            for s in rep_str:            
+                articles = articles.replace(s, "")
+            
+            #replace
+            rep_str = ["\r\n", "　", " "]
+            for s in rep_str:            
+                articles = articles.replace(s, "")
+
+            keywords_Noun = word_sep(id, articles)
+
+            if keywords_Noun == None:
+                continue
+
+            #輸出->案號 詞
+            with open(outpath, 'a', encoding='utf-8') as out:                  
+                for k in keywords_Noun:
+                    out.write(f'{k}\n')
